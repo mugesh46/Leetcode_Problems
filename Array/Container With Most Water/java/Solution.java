9aeee035c288d1a0e602fc5ba06cd324d0b52c38
mugesh46 <mugeshkalaimani8@gmail.com>
2026-06-30 11:01:15 +0530

    feat: Add Container With Most Water

--- Array/Container With Most Water/java/Solution.java
+++ Array/Container With Most Water/java/Solution.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int maxArea(int[] nums) {
-        int max=0;
-        // ans =0;
-      for(int i=0;i<nums.length;i++){
-        for(int j=nums.length-1;j>0;j--){
-            if(nums[i]<nums[j]){
-                        max = Math.max(max, nums[j]);            }
-          int height = Math.min(nums[i], nums[j]);
-                      int width = j - i;
-                      int   area = height * width;
-        max = Math.max(max, area);
-        }
-      }
-      return max;
+  public int maxArea(int[] nums) {
 
+    int max = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = nums.length - 1; j > i; j--) {
+
+            int height = Math.min(nums[i], nums[j]);
+            int width = j - i;
+            int area = height * width;
+
+            max = Math.max(max, area);
+        }
     }
+
+    return max;
+}
+
+    
 }
